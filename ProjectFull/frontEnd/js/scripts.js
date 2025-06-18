95097edf26585028b24d65e0bd61c432d136f4a0
M#TE <100843385+Mute491@users.noreply.github.com>
2025-06-18 12:03:31 +0200

    Delete ProjectFull/frontEnd directory

--- ProjectFull/frontEnd/js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.dropdown-trigger').dropdown({
-        coverTrigger: false    // il dropdown si sovrappone al trigger? (false = sotto)
-    });
-    $('.sidenav').sidenav();
-});
