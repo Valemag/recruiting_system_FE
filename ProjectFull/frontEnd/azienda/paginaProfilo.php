9b14eba57a981603068a90b2a64fec9f227bf77e
Lorenzo Saracino <lorenzosar1234@gmail.com>
2025-06-29 17:37:21 +0200

    Modifica logo e tasto dettagli offerta

--- ProjectFull/frontEnd/azienda/paginaProfilo.php
+++ ProjectFull/frontEnd/azienda/paginaProfilo.php
@@ -17,7 +17,7 @@
 
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" href="../../bitnami.css">
+        <link rel="stylesheet" href="../css/custom.css">
     </head>
     <body class="grey lighten-5">
         <nav>
--- ProjectFull/frontEnd/utente/offerteLavoro.php
+++ ProjectFull/frontEnd/utente/offerteLavoro.php
@@ -19,7 +19,7 @@
 
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" href="../../bitnami.css">
+        <link rel="stylesheet" href="../css/custom.css">
     </head>
     <body class="grey lighten-5">
         <nav>
@@ -80,7 +80,7 @@
                                             <h6 style="margin:0px;" class="black-text">Tipo contratto: ' . $offertaSingola["tipo_contratto"] . '</h6>
                                         </td>
                                         <td>
-                                            <button class="btn light-blue darken-1 modal-trigger" href="#modal' . $index . '" style="margin-right: 10px;">Add commentMore actions
+                                            <button class="btn light-blue darken-1 modal-trigger" href="#modal' . $index . '" style="margin-right: 10px;">
                                                 Mostra dettagli offerta
                                             </button> 
                                             <form method="POST" action="../../backEnd/controllers/utenti/createCandidatura.php" enctype="multipart/form-data" style="display: flex; gap: 10px; align-items: center;">
